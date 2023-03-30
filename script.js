60a112bea60a8fa8f09bdc52b03153db9a7484c5
Nitish Chaurasiya <nitishchaurasiyac555@gmail.com>
2023-03-30 14:08:09 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+       const toggleButton = document.getElementById('toggle');
+        const hamLinks = document.getElementById('ham-links');
+
+        toggleButton.addEventListener('click',() =>{
+            hamLinks.classList.toggle('left-[0%]');
+        })
+
